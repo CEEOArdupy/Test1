188d34df17db454175a65ba66fd6d6097ef98f53
milandahal213 <milandahal213@gmail.com>
2020-11-14 21:31:50 -0500

    Update mod_ardupy_addition.c

--- mod_ardupy_addition.c
+++ mod_ardupy_addition.c
@@ -17,6 +17,8 @@ m_generic_make(add) {
     return self;
 }
 
+
+
 void add_obj_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest){
     abstract_module_t *self = (abstract_module_t *)self_in;
     uint32_t value;
@@ -32,11 +34,22 @@ void add_obj_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest){
     generic_method_lookup(self_in, attr, dest);
 }
 
+mp_obj_t sum(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args){
+    abstract_module_t * self = (abstract_module_t *)(pos_args[0]);
+    int a = mp_obj_get_int(pos_args[1]);
+    int b = mp_obj_get_int(pos_args[2]);
+    mp_obj_t r = common_hal_add_sum(self, a, b);
+    return r;
+}
+ 
+MP_DEFINE_CONST_FUN_OBJ_KW(add_sum_obj, 2, add_sum);
+
 const mp_rom_map_elem_t add_locals_dict_table[] = {
     // instance methods
     { MP_ROM_QSTR(MP_QSTR_deinit),    MP_ROM_PTR(&add_deinit_obj) },
     { MP_ROM_QSTR(MP_QSTR___enter__), MP_ROM_PTR(&default___enter___obj) },
     { MP_ROM_QSTR(MP_QSTR___exit__),  MP_ROM_PTR(&add_obj___exit___obj) },
+     {MP_ROM_QSTR(MP_QSTR_sum), MP_ROM_PTR(&add_sum_obj)},
 };
  
 MP_DEFINE_CONST_DICT(add_locals_dict, add_locals_dict_table);
