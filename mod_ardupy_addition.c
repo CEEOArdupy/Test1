2b62ebf94412cedf3abd7fefe95bf345d21af859
Milan <milandahal213@gmail.com>
2020-11-15 00:23:48 -0500

    Update mod_ardupy_addition.c

--- mod_ardupy_addition.c
+++ mod_ardupy_addition.c
@@ -8,7 +8,7 @@
 
 void common_hal_add_construct(abstract_module_t *self);
 void common_hal_add_deinit(abstract_module_t *self);
-int common_hal_add_sum(abstract_module_t *self, int a , int b);
+float common_hal_add_sum(abstract_module_t *self, float a , float b);
 
 m_generic_make(add) {
     abstract_module_t * self = new_abstruct_module(type);
@@ -19,12 +19,12 @@ m_generic_make(add) {
 
 
 
-int add_sum(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args){
+float add_sum(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args){
     abstract_module_t * self = (abstract_module_t *)(pos_args[0]);
-    int a = mp_obj_get_int(pos_args[1]);
-    int b = mp_obj_get_int(pos_args[2]);
-    int r = common_hal_add_sum(self, a, b);
-    return mp_obj_new_int(r);
+    float a = mp_obj_get_int(pos_args[1]);
+    float b = mp_obj_get_int(pos_args[2]);
+    float r = common_hal_add_sum(self, a, b);
+    return mp_obj_new_float(r);
 }
  
 MP_DEFINE_CONST_FUN_OBJ_KW(add_sum_obj, 2, add_sum);
