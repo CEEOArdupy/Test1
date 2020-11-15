54c79d5931902d649de3baca83421bb676fd70b9
milandahal213 <milandahal213@gmail.com>
2020-11-14 19:59:35 -0500

    Update wrapper_ardupy_addition.cpp

--- wrapper_ardupy_addition.cpp
+++ wrapper_ardupy_addition.cpp
@@ -11,7 +11,7 @@ extern "C"{
 void * operator new(size_t, void *);
 
 extern "C" {
-    void common_hal_add_construct(abstract_module_t *self){
+    bool common_hal_add_construct(abstract_module_t *self){
         self->module = new (m_new_obj(add)) add();
         add.init();
     }
