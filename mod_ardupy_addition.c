6d0fd23adc9564d086a52c823614a4344b0f1fda
milandahal213 <milandahal213@gmail.com>
2020-11-14 19:24:56 -0500

    test

---
+++ mod_ardupy_addition.c
@@ -0,0 +1,53 @@
+#include "py/mphal.h"
+#include "py/nlr.h"
+#include "py/objtype.h"
+#include "py/runtime.h"
+#include "py/obj.h"
+#include "shared-bindings/util.h"
+
+
+void common_hal_add_construct(abstract_module_t *self)
+void common_hal_add_deinit(abstract_module_t *self)
+float common_hal_add_sum(abstract_module_t *self, int a , int b)
+
+m_generic_make(add) {
+    abstract_module_t * self = new_abstruct_module(type);
+    mp_arg_check_num(n_args, n_kw, 0, 0, false);
+    common_hal_add_construct(self);
+    return self;
+}
+
+void add_obj_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest){
+    abstract_module_t *self = (abstract_module_t *)self_in;
+    uint32_t value;
+    float number;
+    if (dest[0] == MP_OBJ_NULL) {
+        if (attr == MP_QSTR_sum) {
+            number = common_hal_sum(self); // Call previously defined unction
+            dest[0] = mp_obj_new_float(number); // This the MicroPython float type, should match with the data type
+            return;
+        }
+        
+    }
+    generic_method_lookup(self_in, attr, dest);
+}
+
+const mp_rom_map_elem_t add_locals_dict_table[] = {
+    // instance methods
+    { MP_ROM_QSTR(MP_QSTR_deinit),    MP_ROM_PTR(&add_deinit_obj) },
+    { MP_ROM_QSTR(MP_QSTR___enter__), MP_ROM_PTR(&default___enter___obj) },
+    { MP_ROM_QSTR(MP_QSTR___exit__),  MP_ROM_PTR(&add_obj___exit___obj) },
+};
+ 
+MP_DEFINE_CONST_DICT(add_locals_dict, add_locals_dict_table);
+
+const mp_obj_type_t milan_add_type = {
+    {&mp_type_type},
+    .name = MP_QSTR_milan_add,
+    .make_new = add_make_new,
+    .locals_dict = (mp_obj_t)&add_locals_dict,
+    .attr = add_obj_attr,
+};
+
+
+
---
+++ wrapper_ardupy_addition.cpp
@@ -0,0 +1,25 @@
+#include "addition/testaddition.h"
+extern "C"{
+#include "py/mphal.h"
+#include "py/nlr.h"
+#include "py/objtype.h"
+#include "py/runtime.h"
+#include "shared-bindings/util.h"
+}
+
+#define add (*(add*)self->module) //class name is add
+void * operator new(size_t, void *);
+
+extern "C" {
+    void common_hal_add_construct(abstract_module_t *self){
+        self->module = new (m_new_obj(add)) add();
+        add.init();
+    }
+   void common_hal_add_deinit(abstract_module_t *self){
+        add.~add();
+    }
+    float common_hal_add_sum(abstract_module_t *self, int a , int b){
+        return add.sum(a,b);
+   
+    }
+}
